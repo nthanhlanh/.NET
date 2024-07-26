0660c67df3b3e39f18c7af55c6ec3f1d0773ef04
lanhnt@unitech.vn <lanhnt@unitech.vn>
2024-07-26 11:13:39 +0700

    remove bin,obj

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d586dddb098c7cf3d4a74d3db24e0f4cd6cb68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d0bbf295da89133c13665fd4076100b68e6be5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
